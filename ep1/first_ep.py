60eb1ad8457355acecf4693c4fb7ce933037668f
NikolayKorolev-droid <nickkorolev201@gmail.com>
2025-05-10 17:04:03 +0300

    Update first_ep.py

--- ep1/first_ep.py
+++ ep1/first_ep.py
@@ -16,8 +16,6 @@ def MakeGraph(filename):
     x = [k[0] for k in points]
     y = [k[1] for k in points]
     fig, ax = plt.subplots(figsize=(10, 10))
-    x_min, x_max = min(x), max(x)
-    y_min, y_max = min(y), max(y)
 
     ax.scatter(x, y, color='blue')
     ax.grid(True, linestyle='-', alpha=0.5)
@@ -28,7 +26,6 @@ def MakeGraph(filename):
     ax.set_aspect('equal')
     output=f"graphic_{filename}.png"
     plt.savefig(output)
-    plt.show()
 
 def main():
     files=['001', '002', '003', '004', '005']
